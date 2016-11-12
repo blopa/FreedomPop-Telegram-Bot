9187480de47519100672087b63daeae1793f9aa3
Pablo Montenegro <montenegro.pablo@outlook.com>
2016-11-12 21:27:12 -0200

    first 'working' version

--- app-test.py
+++ app-test.py
@@ -8,16 +8,17 @@ import tools
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
 
 
-USER_STEP, PASS_STEP, ACCESS = range(3)
+USER_STEP, PASS_STEP, ACCESS, END = range(4)
 API_KEY = sys.argv[1]
 LOGIN = []
-USERS = []
+USERS = [] #tools.User
+EMAIL = '[^@]+@[^@]+\.[^@]+'
 
 
 def main():
 	updater = Updater(API_KEY)
 	dispatcher = updater.dispatcher
-	# tools.db.connect() TODO
+	tools.db.connect()
 	tools.create_tb()
 	getUsers()
 	thread.start_new_thread(checker, ('dunno', 2)) # dunno why I am sending this params
@@ -25,17 +26,23 @@ def main():
 		entry_points=[CommandHandler('start', start)],
 
 		states={
-			USER_STEP: [MessageHandler(Filters.text, user)],
+			USER_STEP: [RegexHandler('^(Add account)$', user)],
 
-			PASS_STEP: [MessageHandler(Filters.text, passw)],
+			PASS_STEP: [RegexHandler(EMAIL, passw)],
 
-			ACCESS: [MessageHandler(Filters.text, access)]
+			ACCESS: [MessageHandler(Filters.text, access)],
+
+			END: [MessageHandler(Filters.text, end)]
 		},
 
 		fallbacks=[CommandHandler('cancel', cancel)]
 	)
 	dispatcher.add_handler(conv_handler)
 	updater.start_polling()
+
+
+def end(bot, update):
+	return ConversationHandler.END
 
 
 def start(bot, update):
@@ -52,13 +59,19 @@ def cancel(bot, update):
 
 
 def user(bot, update):
-	update.message.reply_text('Awesome! Please send me your FreedomPop e-mail')
-
-	return PASS_STEP
+	print 'entrou user'
+	user = update.message.from_user
+	result = tools.User.select().where(tools.User.user_id == user.id)
+	if result:
+		update.message.reply_text('Ops, it seems that you already have an account with us!')
+		return END
+	else:
+		update.message.reply_text('Awesome! Please send me your FreedomPop e-mail')
+		return PASS_STEP
 
 
 def passw(bot, update):
-	LOGIN.append(update.message.text) # add email
+	LOGIN.append(update.message.text)  # add email
 	update.message.reply_text('Great! Now send me the password.')
 
 	return ACCESS
@@ -70,25 +83,34 @@ def access(bot, update):
 	update.message.reply_text('Connecting...')
 
 	userdb = tools.User(name=user.first_name, user_id=user.id, fp_user=LOGIN[0], fp_pass=LOGIN[1])
-	userdb = tools.testConn(userdb.fp_user, userdb.fp_pass)
-	if userdb.initToken():
+	#userdb = tools.testConn(userdb.fp_user, userdb.fp_pass)
+	userdb.getConn()
+	if userdb.api:
 		if userdb.save():
 			USERS.append(userdb)
-		return True
+			update.message.reply_text('We are good to go!')
+		else:
+			update.message.reply_text('something went wrong, try again!')
+		return END
 	else:
-		return False
+		return END
 
 
 def checker(*args, **kwargs):  # this is a thread
 	bot = Bot(API_KEY)
 	while True:
-		for user in USERS:
-			data = user.checkNewSMS(604800)
-			if data:
-				for text in data['messages']:
-					print text['body']
-					bot.sendMessage(chat_id=user.user_id, text=text['body'])
-		time.sleep(30)
+		before = time.time()
+		users = list(USERS)
+		if users:
+			for user in users:
+				data = user.checkNewSMS(604800)
+				if data:
+					for text in data['messages']:
+						print text['body']
+						bot.sendMessage(chat_id=user.user_id, text=text['body'])
+		duration = time.time() - before
+		#time.sleep(30 - (duration * 1000))
+		time.sleep(5)
 
 
 def getUsers():
---
+++ tools.py
@@ -0,0 +1,40 @@
+from fpapi import FreedomPop
+from peewee import *
+import time
+import json
+
+
+db = SqliteDatabase('mydb.db')
+
+class User(Model):
+	name = CharField()
+	user_id = CharField(unique=True)
+	fp_user = CharField(unique=True)
+	fp_pass = CharField(unique=True)
+
+	class Meta:
+		database = db
+
+	def __init__(self, *args, **kwargs):
+		self.api = None
+		super(User, self).__init__(*args, **kwargs)
+
+	def timestamp2Str(self, timestamp):
+		return str(timestamp).replace('.', '').ljust(13, '0')
+
+	def getConn(self):
+		if not self.api:
+			self.api = FreedomPop(self.fp_user, self.fp_pass)
+
+	def checkNewSMS(self, range):  # TODO change from range to read/unread messages
+		self.getConn()
+		currTime = float(time.time())
+		pastTime = currTime - range
+		req = self.api.getSMS(self.timestamp2Str(pastTime), self.timestamp2Str(currTime), False, True)
+		if req.status_code == 200:
+			return json.loads(req.content)
+		else:
+			return False;
+
+def create_tb():
+	User.create_table(True)
